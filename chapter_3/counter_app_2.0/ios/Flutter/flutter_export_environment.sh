12716a340ef03c2219d3614abede73a279b45be4
Silver Lee <agfe2sv+play@gmail.com>
2022-08-10 10:23:27 +0900

    missing style "NormalTheme"

--- chapter_3/counter_app_2.0/ios/Flutter/flutter_export_environment.sh
+++ chapter_3/counter_app_2.0/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/silver/Tool/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/silver/Dev/Ref/GitHub/flutter_in_action_source_code/chapter_3/counter_app_2.0"
+export "FLUTTER_APPLICATION_PATH=/home/silver/Dev/Ref/GitHub/flutter_in_action_source_code/migration/chapter_3/counter_app_2.0"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
